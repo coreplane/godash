121445847afee3f176f2bb3762da4f0ca0636337
Dan Maas <dmaas@maasdigital.com>
2020-12-30 13:36:06 -0800

    Adjust index.d.ts for new-style exports

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-import {
+export {
     BLACK,
     Board,
     Coordinate,
@@ -24,39 +24,8 @@ import {
     toAsciiBoard,
 } from './board';
 
-import {
+export {
     coordinateToSgfPoint,
     sgfPointToCoordinate,
     sgfToJS,
 } from './sgf';
-
-declare const godash: {
-    BLACK: typeof BLACK;
-    WHITE: typeof WHITE;
-    EMPTY: typeof EMPTY;
-    Board: typeof Board;
-    Coordinate: typeof Coordinate;
-    TENGEN_13: typeof TENGEN_13;
-    TENGEN_19: typeof TENGEN_19;
-    TENGEN_9: typeof TENGEN_9;
-    difference: typeof difference;
-    followupKo: typeof followupKo;
-    group: typeof group;
-    oppositeColor: typeof oppositeColor;
-    liberties: typeof liberties;
-    libertyCount: typeof libertyCount;
-    isLegalMove: typeof isLegalMove;
-    removeStone: typeof removeStone;
-    removeStones: typeof removeStones;
-    addMove: typeof addMove;
-    placeStone: typeof placeStone;
-    placeStones: typeof placeStones;
-    toAsciiBoard: typeof toAsciiBoard;
-    constructBoard: typeof constructBoard;
-    handicapBoard: typeof handicapBoard;
-    coordinateToSgfPoint: typeof coordinateToSgfPoint;
-    sgfPointToCoordinate: typeof sgfPointToCoordinate;
-    sgfToJS: typeof sgfToJS;
-};
-
-export = godash;
